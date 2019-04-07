9412a62f8cf75f299cade25f670238f9a8b33e0d
davalcato <davalcato@yahoo.com>
2019-04-07 12:01:49 -0700

    Implemented the collectionView.register

--- Twitus/HomeController.swift
+++ Twitus/HomeController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class HomeController: UICollectionViewController {
+    
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .green
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        
+        cell.backgroundColor = .blue
+        return cell
     }
 }
 
