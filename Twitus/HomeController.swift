af97c0de0463769d0c4f121dd63b38a350c9a59e
davalcato <davalcato@yahoo.com>
2019-04-07 11:17:15 -0700

    Added the UICollectionViewFlowLayout to the UICollectionView to change the color to green

---
+++ Twitus/HomeController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeController.swift
+//  Twitus
+//
+//  Created by Daval Cato on 4/7/19.
+//  Copyright © 2019 Daval Cato. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .green
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
