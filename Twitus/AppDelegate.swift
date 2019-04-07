35f3e167b397c4e0b6d6328746fc9ee32fc0dbbf
davalcato <davalcato@yahoo.com>
2019-04-07 11:00:48 -0700

    Added the window?.makeKeyAndVisible()

--- Twitus/AppDelegate.swift
+++ Twitus/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //ignore the storyboard
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
