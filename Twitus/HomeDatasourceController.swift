72ee2bb3ffe7445980844ff7bb83e71ffacbf3c1
davalcato <davalcato@yahoo.com>
2019-04-09 20:32:50 -0700

    introduce numberOfItems

--- Twitus/HomeDatasourceController.swift
+++ Twitus/HomeDatasourceController.swift
@@ -8,10 +8,22 @@
 
 import LBTAComponents
 
+class HomeDatasource: Datasource {
+    
+    let words = ["user1", "user2", "user3"]
+
+    override func numberOfItems(_ section: Int) -> Int {
+        return words.count
+    }
+    
+    
+}
+
 class HomeDatasourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .red
+       
+        
     }
 }
 
