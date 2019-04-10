f8d0c9b2d2ff46f4949f0f998f73afb40d460f8f
davalcato <davalcato@yahoo.com>
2019-04-09 21:21:12 -0700

    added the nameLabel.anchor(topAnchor

--- Twitus/HomeDatasourceController.swift
+++ Twitus/HomeDatasourceController.swift
@@ -9,9 +9,20 @@
 import LBTAComponents
 
 class UserCell: DatasourceCell {
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TEST TEST TEST"
+        return label
+    }()
+    
     override func setupViews() {
         super.setupViews()
         backgroundColor = .yellow
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
     }
 }
 
