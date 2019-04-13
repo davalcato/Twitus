382e59671b038e9cf47de741cb59b7b2e378aa8f
davalcato <davalcato@yahoo.com>
2019-04-12 20:23:09 -0700

    added the profileImageView that creates the image boxes

--- Twitus/View/Cell.swift
+++ Twitus/View/Cell.swift
@@ -31,6 +31,13 @@ class UserCell: DatasourceCell {
         
     }
     
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+        
+    }()
+    
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "TEST TEST TEST"
@@ -40,6 +47,16 @@ class UserCell: DatasourceCell {
     override func setupViews() {
         super.setupViews()
         backgroundColor = .yellow
+        
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12
+            , leftConstant: 12
+            , bottomConstant: 0
+            , rightConstant: 0
+            , widthConstant: 50
+            , heightConstant: 50)
+        
         
         addSubview(nameLabel)
         nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
